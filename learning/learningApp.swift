0c09fecc9a677d278041cb435d61c5a5fbad5a10
Fabriano28 <Farrel.Brian@bni.co.id>
2025-09-22 08:56:57 +0700

    Initial Commit

---
+++ learning/learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningApp.swift
+//  learning
+//
+//  Created by Farrel Brian Rafi on 22/09/25.
+//
+
+import SwiftUI
+
+@main
+struct learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
